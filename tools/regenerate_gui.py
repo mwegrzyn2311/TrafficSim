84f051f5d0d409062b2696504b82fae81672f40a
Xert <xertdev@gmail.com>
2022-03-12 17:40:23 +0100

    Fix ui generation script

--- tools/regenerate_gui.py
+++ tools/regenerate_gui.py
@@ -5,9 +5,12 @@ BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 RESOURCE_PATH = BASE_PATH + "/../resources"
 OUTPUT_PATH = BASE_PATH + "/../gui/generated"
 
+os.makedirs(OUTPUT_PATH, exist_ok=True)
+
 for root, dirs, files in os.walk(RESOURCE_PATH):
 	for name in files:
 		if name.lower().endswith(".ui"):
 			basename = name[0:-3]
-			print(basename)
 			os.system("pyside6-uic {1}/{0}.ui > {2}/ui_{0}.py".format(basename, RESOURCE_PATH, OUTPUT_PATH))
+
+os.system("touch " + OUTPUT_PATH + "/__init__.py")
