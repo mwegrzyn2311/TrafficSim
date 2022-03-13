03dbeeb95f388e4034858cff59a8f1b44983aed2
Michal23 <michal.wegrzyn2137@gmail.com>
2022-03-13 16:15:35 +0100

    Fix naming

--- model/intersections/intersection_with_signs.py
+++ model/intersections/intersection_with_signs.py
@@ -3,6 +3,6 @@ from model import Vec2d, Node
 from typing import List
 
 
-class IntersectionUnmarked(Intersection):
+class IntersectionWithSigns(Intersection):
     def __init__(self, pos: Vec2d, nodes_in: List[Node], nodes_out: List[Node]):
         super().__init__(pos, nodes_in, nodes_out)
